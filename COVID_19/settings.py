b1014c336cb677b7a14adca812e15a225bd16ab7
Jihang Li <jihangli@Jihangs-MacBook-Air.local>
2020-10-30 08:54:15 -0700

    addinghost

--- COVID_19/settings.py
+++ COVID_19/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+4nk*sa!+dxnd^!-^b^_+-ie_c9gzdr5^tjy$2pu859bt9wb-a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['easycoviddata.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
